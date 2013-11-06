7eacdae88fc29c3fdd3a61b86e731e72721bda31
Josh <jscanish@gmail.com>
2013-11-05 20:39:24 -0600

    rspec tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,9 +2,32 @@ require 'spec_helper'
 
 
 describe QuestionsController do
-  describe "POST update" do
-    it "updates user's questions if user is current user"
-    it "does not update questions for other users"
-    it "redirects to user path"
+  describe "PUT update" do
+    it "updates user's questions if user is current user" do
+      user = Fabricate(:user)
+      session[:user_id] = user.id
+      question = Question.create(self_summary: "hi", user: user)
+      @attr = { self_summary: "new summary" }
+      put :update, id: question.id, user_id: user.id, question: @attr
+      question.reload
+      expect(question.self_summary).to eq("new summary")
+    end
+    it "does not update questions for other users" do
+      user = Fabricate(:user)
+      question = Question.create(self_summary: "hi", user: user)
+      @attr = { self_summary: "new summary" }
+      put :update, id: question.id, user_id: user.id, question: @attr
+      question.reload
+      expect(question.self_summary).to eq("hi")
+    end
+    it "redirects to user path" do
+      user = Fabricate(:user)
+      session[:user_id] = user.id
+      question = Question.create(self_summary: "hi", user: user)
+      @attr = { self_summary: "new summary" }
+      put :update, id: question.id, user_id: user.id, question: @attr
+      question.reload
+      expect(response).to redirect_to user_path
+    end
   end
 end
